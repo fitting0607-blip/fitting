c2ff594c99689fc1cc05cc195e7ea1d694d8ef25
fitting <fitting0607@gmail.com>
2026-04-14 15:32:01 +0900

    feat: Enhance authentication flow with session persistence and user redirection

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,25 @@
+import { Stack } from 'expo-router';
+
+export default function AuthLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerTitleAlign: 'center',
+      }}
+    >
+      <Stack.Screen
+        name="login"
+        options={{
+          title: '로그인',
+        }}
+      />
+      <Stack.Screen
+        name="register"
+        options={{
+          title: '회원가입',
+        }}
+      />
+    </Stack>
+  );
+}
+
